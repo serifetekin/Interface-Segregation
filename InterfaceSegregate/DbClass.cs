eeb27b97b6b596d1d8f4da5dec1f619a53847125
Şerife Tekin <serifetekin2451@gmail.com>
2020-06-05 16:07:51 +0300

    Solid prensiplerinden Interface Segregation Prensibi

---
+++ InterfaceSegregate/DbClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregate
+{
+    interface IDbClass
+    {
+        void TabloGetir(string sql);
+        void SQLCalistir(string sql);
+    }
+
+    interface ISqlServer
+    {
+        void SPCalistir(string sql);
+    }
+
+    class DbAccess : IDbClass
+    {
+
+        public void SQLCalistir(string sql)
+        {
+            Console.WriteLine("sql çalıştı");
+        }
+
+        public void TabloGetir(string sql)
+        {
+            Console.WriteLine("tablo getirildi");
+        }
+    }
+    class DbSqlServer : IDbClass, ISqlServer
+    {
+        public void SPCalistir(string sql)
+        {
+            Console.WriteLine("sp çalıştı");
+        }
+
+        public void SQLCalistir(string sql)
+        {
+            Console.WriteLine("sql çalıştı");
+        }
+
+        public void TabloGetir(string sql)
+        {
+            Console.WriteLine("tablo getirildi");
+        }
+    }
+}
---
+++ InterfaceSegregate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbAccess dba = new DbAccess();
+            dba.SQLCalistir("SQL");
+
+            DbSqlServer dbs = new DbSqlServer();
+            dbs.SQLCalistir("sql");
+            dbs.SPCalistir("sql");
+
+            Console.ReadLine();
+        }
+    }
+}
